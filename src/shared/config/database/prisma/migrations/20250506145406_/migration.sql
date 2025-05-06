8b63f874a55fbd5ff0c56a091407d5aa432e6484
Cassio <cassiojr0108@gmail.com>
2025-05-06 12:14:07 -0300

    modifications

--- src/shared/config/database/prisma/migrations/20250506145406_/migration.sql
+++ src/shared/config/database/prisma/migrations/20250506145406_/migration.sql
@@ -10,7 +10,7 @@ CREATE TYPE "Privacy" AS ENUM ('PUBLIC', 'PRIVATE');
 -- CreateTable
 CREATE TABLE "User" (
     "userId" TEXT NOT NULL,
-    "user" VARCHAR(255) NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
     "email" TEXT NOT NULL,
     "password" VARCHAR(255) NOT NULL,
     "birthDate" TIMESTAMP(3),
