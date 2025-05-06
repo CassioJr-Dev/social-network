db5015df31060d2784410b03390500afe9d7530b
Cassio <cassiojr0108@gmail.com>
2025-05-06 11:15:40 -0300

    modification

--- src/shared/config/database/generated/prisma/edge.js
+++ src/shared/config/database/generated/prisma/edge.js
@@ -230,7 +230,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../../../../.env"
   },
   "relativePath": "../../prisma",
@@ -240,11 +240,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:docker@localhost:5432/social-network-db\n?schema=public"
+        "value": null
       }
     }
   },
--- src/shared/config/database/generated/prisma/index.js
+++ src/shared/config/database/generated/prisma/index.js
@@ -231,7 +231,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../../../../.env"
   },
   "relativePath": "../../prisma",
@@ -241,11 +241,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:docker@localhost:5432/social-network-db\n?schema=public"
+        "value": null
       }
     }
   },
