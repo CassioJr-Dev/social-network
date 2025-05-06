780621fc254ce18930a717a7cc1ad26e1050ffa5
Cassio <cassiojr0108@gmail.com>
2025-05-06 12:09:01 -0300

    feat(UserDao): adding userDao

---
+++ src/user/models/dao/user.dao.ts
@@ -0,0 +1,47 @@
+import { PrismaClient } from "@prisma/client";
+import { UserEntity } from "../entities/user.entity";
+
+export class UserDao {
+  constructor(private prismaService: PrismaClient){}
+
+  async createUser(user: UserEntity): Promise<UserEntity> {
+    return this.prismaService.user.create({
+      data: user
+    })
+  }
+
+  async updateUser(user: Partial<UserEntity>): Promise<UserEntity> {
+    const update = await this.prismaService.user.update({
+      data: user,
+      where: {
+        userId: user.userId
+      }
+    })
+
+    return update
+  }
+
+  async findUserById(userId: string): Promise<UserEntity | null> {
+    return await this.prismaService.user.findUnique({
+      where: {
+        userId
+      }
+    })
+  }
+
+  async findUserByEmail(email: string): Promise<UserEntity | null> {
+    return await this.prismaService.user.findUnique({
+      where: {
+        email
+      }
+    })
+  }
+
+  async deleteUser(userId: string): Promise<void> {
+    await this.prismaService.user.delete({
+      where: {
+        userId
+      }
+    })
+  }
+}
