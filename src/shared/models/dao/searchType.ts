bf6f3b50643add4ac3e547069158b1bbc22a6742
Cassio <cassiojr0108@gmail.com>
2025-05-10 18:58:43 -0300

    SearchType

---
+++ src/shared/models/dao/searchType.ts
@@ -0,0 +1,16 @@
+export type ISearchParams<Filter = string> = {
+  page: number
+  perPage: number
+  sort: string
+  sortDir: string
+  filter: Filter
+}
+
+export type ISearchResult<T> = {
+  items: T[]
+  total: number
+  currentPage: number
+  sort: string
+  perPage: number
+  sortDir: string
+}
