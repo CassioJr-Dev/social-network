192c256fa31cdf740472e917c46d1d22cd4c0dd0
Cassio <cassiojr0108@gmail.com>
2025-05-10 18:59:03 -0300

    PostDao

---
+++ src/post/models/dao/post.dao.ts
@@ -0,0 +1,70 @@
+import { PrismaClient } from '@prisma/client'
+import { IPostDao } from './interface.dao'
+import { PostEntity } from '../entities/post.entity'
+import {
+  ISearchParams,
+  ISearchResult,
+} from '@/shared/models/dao/searchType'
+
+export class PostDao implements IPostDao {
+  readonly sortableFields: string[] = ['title', 'createdAt']
+
+  constructor(private prismaService: PrismaClient) {}
+
+  async createPost(post: PostEntity): Promise<PostEntity> {
+    return this.prismaService.post.create({
+      data: post,
+    })
+  }
+  async updatePost(post: Partial<PostEntity>): Promise<PostEntity> {
+    return this.prismaService.post.update({
+      where: { postId: post.postId },
+      data: post,
+    })
+  }
+  async findPostById(postId: string): Promise<PostEntity | null> {
+    return this.prismaService.post.findUnique({
+      where: { postId },
+    })
+  }
+  async deletePost(postId: string): Promise<void> {
+    await this.prismaService.post.delete({
+      where: { postId },
+    })
+  }
+
+  async search(
+    authorId: string,
+    props: ISearchParams,
+  ): Promise<ISearchResult<PostEntity>> {
+    const sortable = this.sortableFields?.includes(props.sort) || false
+    const orderByField = sortable ? props.sort : 'createdAt'
+    const orderByDir = sortable ? props.sortDir : 'desc'
+
+    const count = await this.prismaService.post.count({
+      where: {
+        authorId,
+      },
+    })
+
+    const models = await this.prismaService.post.findMany({
+      where: {
+        authorId,
+      },
+      orderBy: {
+        [orderByField]: orderByDir,
+      },
+      skip: props.page && props.page > 0 ? (props.page - 1) * props.perPage : 1,
+      take: props.perPage && props.perPage > 0 ? props.perPage : 15,
+    })
+
+    return {
+      items: models,
+      total: count,
+      currentPage: props.page,
+      perPage: props.perPage,
+      sort: orderByField,
+      sortDir: orderByDir,
+    }
+  }
+}
