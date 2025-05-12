88b201fe9e74e9b8a36983b09be564e06195be79
Cassio <cassiojr0108@gmail.com>
2025-05-12 12:01:02 -0300

    rename interfaceUser

---
+++ src/user/models/dao/interfaceUser.dao.ts
@@ -0,0 +1,9 @@
+import { UserEntity } from '../entities/user.entity'
+
+export interface IUserDao {
+  createUser(user: UserEntity): Promise<UserEntity>
+  updateUser(user: Partial<UserEntity>): Promise<UserEntity>
+  findUserById(userId: string): Promise<UserEntity | null>
+  findUserByEmail(email: string): Promise<UserEntity | null>
+  deleteUser(userId: string): Promise<void>
+}
